cfa8cde67ab06406327ee61d17e32e9f41da6fcb
ye <15888520997@sina.cn>
2023-11-15 11:24:37 +0800

    add init db tables

--- core/init.go
+++ core/init.go
@@ -1,1 +1,26 @@
 package core
+
+import (
+	"log"
+	"me/core/db"
+)
+
+
+func CreateMeAllTable(){
+	database, err := db.ConnDb()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	createUserTable := `CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, userName TEXT, password TEXT);`
+	_, err = database.Exec(createUserTable)
+	if err != nil {
+		log.Fatal("ERROR: create table cron fail,", err.Error())
+	}
+	err = db.SetMeUserAndPasswd()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	//第二张表
+	// ......
+	return
+}
