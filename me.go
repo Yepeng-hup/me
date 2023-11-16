66961b4273f4c51854b65683e2741dbececb8ce2
ye <15888520997@sina.cn>
2023-11-16 13:12:45 +0800

    update file

--- me.go
+++ me.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"me/core"
+	"me/core/db"
 	"me/global"
 	"me/route"
 )
@@ -14,11 +15,8 @@ var (
 func main(){
 	core.InitJsonFile(jsonPath)
 	r := route.InitRoute()
-	err := core.CreateMeAllTable()
-	if err != nil{
-		log.Fatal(err)
-	}
-
+	core.CreateMeAllTable()
+	db.SelectSetUser()
 	if err := r.Run(global.Cfg.Me.Ip+":"+global.Cfg.Me.Port); err != nil {
 		log.Println("ERROR: error start fail", err)
 	}
