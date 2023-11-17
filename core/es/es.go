a6f1bafd10cad2ff548702d6d17fa3d6a85b11a5
ye <15888520997@sina.cn>
2023-11-17 14:51:43 +0800

    add conn es

--- core/es/es.go
+++ core/es/es.go
@@ -1,1 +1,22 @@
 package es
+
+import (
+	"github.com/elastic/go-elasticsearch/v8"
+	"me/core/logs"
+	"me/global"
+)
+
+func EsConn() *elasticsearch.Client {
+	// new es clien config
+	NewEsConf := elasticsearch.Config{
+		Addresses: []string{global.Cfg.Elasticsearch.Ssl+"://"+global.Cfg.Elasticsearch.Ip+":"+global.Cfg.Elasticsearch.Port},
+	}
+
+	// new es client
+	es, err := elasticsearch.NewClient(NewEsConf)
+	if err != nil {
+		logs.Errorf(err.Error())
+		return nil
+	}
+	return es
+}
